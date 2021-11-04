b4672094c05e6761ce9d3953335269efae423043
GIOhnny <giohnny@gmail.com>
2021-11-04 23:36:47 +0200

    @Autowired @Service @Repository

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -1,8 +1,8 @@
 package main;
 
-import beans.MyBean;
 import config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import services.ProductDeliveryService;
 
 public class main {
     /**
@@ -13,14 +13,9 @@ public class main {
         //contextul se configureaza cu o clasa de configurare pusa cu @Configutation
         try (var context =
                      new AnnotationConfigApplicationContext(ProjectConfig.class)) {
-            //request by Type
-            MyBean b1 = context.getBean(MyBean.class);
-            MyBean b2 = context.getBean(MyBean.class);
-            MyBean b3 = context.getBean(MyBean.class);
 
-            System.out.println(b1.getText());
-            System.out.println(b2.getText());
-            System.out.println(b3.getText());
+            ProductDeliveryService service = context.getBean(ProductDeliveryService.class);
+            service.addSomeProducts();
         }
     }
 }
